65f8e178569ebfa6f66cd132a572c006135c059c
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-07-19 23:18:24 +0300

    or1k specific config.sh

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@
 #ARCH=microblaze
 #ARCH=mips
 #ARCH=mipsel
+ARCH=or1k
 
 CC_BASE_PREFIX=/opt/cross
 
@@ -24,3 +25,15 @@ MAKEFLAGS=-j8
 
 # Enable this to build the bootstrap gcc (thrown away) without optimization, to reduce build time
 GCC_STAGE1_NOOPT=1
+
+BINUTILS_URL=ftp://sourceware.org/pub/binutils/snapshots/binutils-@VERSION@.tar.bz2
+GCC_URL=https://github.com/openrisc/or1k-gcc/archive/musl-4.9.0.tar.gz
+GCC_EXTRACT_DIR=or1k-gcc-musl-4.9.0
+GCC_VERSION=or1k-4.9.0
+
+MUSL_GIT_VERSION=master
+MUSL_GIT_REPO='git://git.musl-libc.org/musl'
+MUSL_VERSION="$MUSL_GIT_VERSION"
+MUSL_GIT=yes
+
+LINUX_HEADERS_URL=http://www.kernel.org/pub/linux/kernel/v3.x/linux-3.15.tar.xz
